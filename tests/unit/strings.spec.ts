c48a41b2126399c78761e47237431a67333edac7
Kieran Simpson <kierans777@gmail.com>
2019-09-09 22:38:49 +1000

    Add string utility to generate getters/setter names.

--- tests/unit/strings.spec.ts
+++ tests/unit/strings.spec.ts
@@ -1,9 +1,17 @@
 import { assertThat, is } from "hamjest";
 
-import { capitalise } from "@/strings";
+import { capitalise, getter, setter } from "@/strings";
 
 describe("strings", function() {
 	it("should capitalise string", function() {
 		assertThat(capitalise("hello world"), is("Hello World"));
+	});
+
+	it("should create getter from prop", function() {
+		assertThat(getter("center"), is("getCenter"));
+	});
+
+	it("should create setter from prop", function() {
+		assertThat(setter("center"), is("setCenter"));
 	});
 });
